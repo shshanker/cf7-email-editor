67f7ff07920db2d58cbe438ffa427b7da381406b
shshanker <shivashankerbhatta@gmail.com>
2016-09-26 12:12:17 +0545

    replaced style/link tags

--- includes/cf7ee-backend.php
+++ includes/cf7ee-backend.php
@@ -30,45 +30,35 @@ class cf7ee_backend {
 
 		// Get the necessary scripts to launch tinymce
 		
-
-
 		add_action('admin_enqueue_scripts', array($this, 'cf7_enqueue_scripts_backend') );
 		add_action( 'wp_print_footer_scripts', array( '_WP_Editors', 'editor_js' ), 50 );
 		add_action( 'wp_print_footer_scripts', array( '_WP_Editors', 'enqueue_scripts' ), 1 );	
 
-		add_action('admin_footer', array($this, 'cf7ee_print_footer_scripts'));
 	}
 
 	
 
 
 	public function cf7_enqueue_scripts_backend(){
-		$cssurl = includes_url('css/');
-		$css = $cssurl . 'editor.css';
-		wp_register_style('tinymce_css', $css);
-		wp_enqueue_style('tinymce_css');
-
-	}
-
-	public function cf7ee_print_footer_scripts(){
 		// Get the necessary scripts to launch tinymce
 		$baseurl = includes_url( 'js/tinymce' );
 		$cssurl = includes_url('css/');
-
-		global $tinymce_version, $concatenate_scripts, $compress_scripts;
-
-		$version = 'ver=' . $tinymce_version;
 		$css = $cssurl . 'editor.css';
+		global $tinymce_version, $concatenate_scripts, $compress_scripts;	
+		
+		wp_register_style('tinymce_css', $css);
+		wp_enqueue_style('tinymce_css');
 
-		$compressed = $compress_scripts && $concatenate_scripts && isset($_SERVER['HTTP_ACCEPT_ENCODING'])
-		    && false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip');
+		$compressed = $compress_scripts && $concatenate_scripts && isset($_SERVER['HTTP_ACCEPT_ENCODING']) && false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip');
 
-		if ( $compressed ) {
-		    echo "<script type='text/javascript' src='{$baseurl}/wp-tinymce.php?c=1&amp;$version'></script>\n";
+		if ( $compressed ) {		    
+	        wp_enqueue_script( 'cf7ee_wp_tiny_mce_php', $baseurl . '/wp-tinymce.php?c=1&amp;', array(), $tinymce_version, true );
 		} else {
-		    echo "<script type='text/javascript' src='{$baseurl}/tinymce.min.js?$version'></script>\n";
-		    echo "<script type='text/javascript' src='{$baseurl}/plugins/compat3x/plugin.min.js?$version'></script>\n";		    
+		    wp_enqueue_script( 'cf7ee_wp_tiny_mce_min', $baseurl . '/tinymce.min.js', array(), $tinymce_version, true );
+		    wp_enqueue_script( 'cf7ee_wp_tiny_mce_plugins', $baseurl . '/plugins/compat3x/plugin.min.js', array(), $tinymce_version, true );		    
 		}
+
+
 
 	}
 	
